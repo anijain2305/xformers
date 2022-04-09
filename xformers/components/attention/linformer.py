b77ed798921e8e6723f07d00cff5e681ce2911fc
Benjamin Lefaudeux <benjamin.lefaudeux@pm.me>
2022-04-09 14:53:12 -0700

    tentative CI fix

--- xformers/components/attention/linformer.py
+++ xformers/components/attention/linformer.py
@@ -47,9 +47,6 @@ class LinformerAttention(Attention):
         # kq need to have the same dimension
         self.requires_same_k_q_dimensions = True
 
-        # THis attention has a specific input projection mechanism
-        self.requires_input_projection = False
-
         # Properties specific to this attention mechanism
         self.supports_attention_mask = False
 
